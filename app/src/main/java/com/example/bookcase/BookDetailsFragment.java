1113f778842c8b9076aaa715d0772a39615a6439
Theodore <tuf94849@temple.edu>
2019-03-23 18:19:28 -0400

    Creating the 2 fragments

---
+++ app/src/main/java/com/example/bookcase/BookDetailsFragment.java
@@ -0,0 +1,29 @@
+package com.example.bookcase;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookDetailsFragment extends Fragment {
+
+
+    public BookDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book_details, container, false);
+    }
+
+}
