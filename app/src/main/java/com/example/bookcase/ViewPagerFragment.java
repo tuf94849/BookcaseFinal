1fd28e994b0387cff9a694d08ad135861a831dd8
Theodore <tuf94849@temple.edu>
2019-03-26 19:08:55 -0400

    Moving book array to strings resource xml file

--- app/src/main/java/com/example/bookcase/ViewPagerFragment.java
+++ app/src/main/java/com/example/bookcase/ViewPagerFragment.java
@@ -3,9 +3,15 @@ package com.example.bookcase;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,11 +25,44 @@ public class ViewPagerFragment extends Fragment {
     }
 
 
+    ViewPager vp;
+    BookDetailsFragment bdFragment;
+
+
+    PagerAdapter pagerAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+        View v = inflater.inflate(R.layout.fragment_view_pager, container, false);
+        vp = v.findViewById(R.id.viewPager);
+
+
+        return v;
+    }
+
+    class PagerAdapter extends FragmentStatePagerAdapter {
+
+        ArrayList<BookDetailsFragment> pagerFragments;
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+            pagerFragments = new ArrayList<>();
+        }
+
+        public void add(BookDetailsFragment fragment){
+            pagerFragments.add(fragment);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return pagerFragments.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return pagerFragments.size();
+        }
     }
 
 }
