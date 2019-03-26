4e79bb5e61e478273bd3417635d1e1bc9db3f76e
Theodore <tuf94849@temple.edu>
2019-03-26 18:44:43 -0400

    BookDetails fragment

--- app/src/main/java/com/example/bookcase/BookDetailsFragment.java
+++ app/src/main/java/com/example/bookcase/BookDetailsFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,13 +19,38 @@ public class BookDetailsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    TextView tv;
+    String bookPicked;
+    public static final String BOOK_KEY = "bookTitle";
+
+    public static BookDetailsFragment newInstance(String book) {
+        BookDetailsFragment fragment = new BookDetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(BOOK_KEY, book);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getArguments() != null){
+            bookPicked = getArguments().getString(BOOK_KEY);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_book_details, container, false);
+        tv = getView().findViewById(R.id.tvTitle);
+        tv.setText(bookPicked);
+
         return v;
     }
+
+
+
 
 }
