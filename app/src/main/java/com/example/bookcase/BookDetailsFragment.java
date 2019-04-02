a903f9b362cf1267a92f0d27ee64bfd09cbe2a18
Theodore <tuf94849@temple.edu>
2019-04-02 18:25:27 -0400

    Book details fragment changed for getting info from web

--- app/src/main/java/com/example/bookcase/BookDetailsFragment.java
+++ app/src/main/java/com/example/bookcase/BookDetailsFragment.java
@@ -6,7 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -21,6 +26,12 @@ public class BookDetailsFragment extends Fragment {
 
     TextView tv;
     String bookPicked;
+    Button btn;
+    ImageView imageView;
+    EditText editText;
+    String title;
+    String author;
+    int publishYear;
     public static final String BOOK_KEY = "bookTitle";
 
     public static BookDetailsFragment newInstance(String book) {
@@ -44,17 +55,34 @@ public class BookDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_book_details, container, false);
-        tv = v.findViewById(R.id.tvTitle);
+        tv = v.findViewById(R.id.bookTitle);
+
+        imageView = v.findViewById(R.id.bookImage);
+        btn = v.findViewById(R.id.btnSearch);
+        editText = v.findViewById(R.id.searchBar);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchBook = editText.getText().toString();
+
+            }
+        });
         //tv.setText(bookPicked);
-        bookPicked(bookPicked);
+        //bookPicked(bookPicked);
 
         return v;
     }
 
 
-    public void bookPicked (String bookPicked){
-        //Log.d("Book", bookSelected);
-        tv.setText(bookPicked);
+    public void bookPicked (Book book){
+        author = book.getAuthor();
+        title = book.getTitle(); publishYear = book.getPublished();
+        tv.setText(" \"" + title + "\" ");
+        tv.append(", " + author);
+        tv.append(", " + publishYear);
+        String imageURL = book.getCoverURL();
+        Picasso.get().load(imageURL).into(imageView);
     }
 
 
